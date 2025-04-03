0a178aa5427812c7408a75067ecebe671cfea221
yinyuanzhang <yinyuanzhang16@163.com>
2025-04-04 02:02:46 +0800

    fix

--- scripts/v1_5/finetune_lora.sh
+++ scripts/v1_5/finetune_lora.sh
@@ -2,7 +2,7 @@
 
 deepspeed llava/train/train_mem.py \
     --lora_enable True --lora_r 128 --lora_alpha 256 --mm_projector_lr 2e-5 \
-    --deepspeed ./scripts/zero3_offload.json \
+    --deepspeed ./scripts/zero3.json \
     --model_name_or_path lmsys/vicuna-7b-v1.5 \
     --version v1 \
     --data_path /data02/gta/playground/data/llava_v1_5_mix665k.json \
@@ -18,9 +18,9 @@ deepspeed llava/train/train_mem.py \
     --bf16 True \
     --output_dir ./checkpoints/llava-v1.5-7b-lora \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 4 \
+    --per_device_train_batch_size 2 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 4 \
+    --gradient_accumulation_steps 8 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 50000 \
